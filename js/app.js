da290b62baf281b28747aa382a2f8c3183d935ca
Sebastian Vasquez <sebitas0489@gmail.com>
2023-12-29 22:29:07 -0500

    Event listener para los elementos select de busqueda

--- js/app.js
+++ js/app.js
@@ -1,9 +1,28 @@
 // Variables
-const resultado = document.querySelector('#resultado');
+const marca = document.querySelector('#marca');
 const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+
+// Contenedor para los resultados
+const resultado = document.querySelector('#resultado');
 
 const max = new Date().getFullYear(); //lista los años
 const min = max -10;
+
+// Generar un objeto con la busqueda
+const datosBusqueda = {
+  marca : '',
+  year : '',
+  minimo : '',
+  maximo : '',
+  puertas : '',
+  transmision : '',
+  color : '',
+}
 
 
 // Eventos
@@ -14,20 +33,39 @@ document.addEventListener('DOMContentLoaded', () => {
   llenarSelect();
 })
 
+// Event listener para los selec de busqueda
+marca.addEventListener('change', e => {
+  datosBusqueda.marca = e.target.value;
+})
+year.addEventListener('change', e => {
+  datosBusqueda.year = e.target.value;
+})
+minimo.addEventListener('change', e => {
+  datosBusqueda.minimo = e.target.value;
+})
+maximo.addEventListener('change', e => {
+  datosBusqueda.maximo = e.target.value;
+})
+puertas.addEventListener('change', e => {
+  datosBusqueda.puertas = e.target.value;
+})
+transmision.addEventListener('change', e => {
+  datosBusqueda.transmision = e.target.value;
+})
+color.addEventListener('change', e => {
+  datosBusqueda.color = e.target.value;
+  console.log(datosBusqueda);
+})
 
 // Funciones
 function mostrarAutos() {
-
   autos.forEach(auto => {
-
     const { marca, modelo, year, puertas, transmision, precio, color } = auto;
     const autoHTML = document.createElement('p');
     autoHTML.textContent = `
-    ${marca} ${modelo} - ${year} - ${puertas} Puertas - Trasmisión: ${transmision} - Precio: ${precio} - Color: ${color}
-
-    `
+      ${marca} ${modelo} - ${year} - ${puertas} Puertas - Trasmisión: ${transmision} - Precio: ${precio} - Color: ${color}`
   
-    // Insertar html
+  // Insertar html
     resultado.appendChild(autoHTML)
   });
 }
@@ -36,10 +74,8 @@ function mostrarAutos() {
 function llenarSelect() {
   for (let i = max; i >= min; i--) {
     const opcion = document.createElement('option');
-    opcion.value = i;
-    opcion.textContent = i;
-
-    year.appendChild(opcion); //Agregar las opciones de año al select.
-    
+      opcion.value = i;
+      opcion.textContent = i;
+        year.appendChild(opcion); //Agregar las opciones de año al select.
   }
 }
